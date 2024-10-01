ba4e2c864926dafd8c093478eb1245f56dae7662
Noumanawaz <114594877+Noumanawaz@users.noreply.github.com>
2024-09-30 23:06:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function openWebpage() {
+  var url = './sub-folder/Projects.html';
+  window.open(url, '_self');
+}
+
+function openDora() {
+  var url = 'https://grayhat-3d-objects.dora.run/';
+  window.open(url, '_blank');
+}
+
+function openHomepage() {
+  var url = '../index.html';
+  window.open(url, '_self');
+}
+
